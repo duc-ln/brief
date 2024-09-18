7e825b6420c5f64d6539b95f9de7fe430559824b
mume_ducln <ducln@mumesoft.com>
2024-09-19 00:19:49 +0700

    feat: create our service block and button can scroll down

--- wp-content/themes/themeCustom/assets/js/module.js
+++ wp-content/themes/themeCustom/assets/js/module.js
@@ -6,17 +6,33 @@ const Modules = {
 		try {
 			const elementScroll = document.querySelector("body");
 			const headerEl = document.querySelector("header");
+
+			const canScrollDownButtonEl = document.querySelector(
+				"#can-scroll-down-button",
+			);
 			if (elementScroll) {
-				elementScroll.onscroll = () => {
+				elementScroll.onscroll = (e) => {
 					const offsetTop = window.pageYOffset;
 					if (offsetTop === 0) headerEl.classList.remove("active");
 					if (offsetTop > 0) headerEl.classList.add("active");
+
+					// Handle display can scroll down button
+					const scrollTop =
+						window.pageYOffset ||
+						document.documentElement.scrollTop ||
+						document.body.scrollTop;
+					const clientHeight = document.documentElement.clientHeight;
+					const scrollHeight = document.documentElement.scrollHeight;
+
+					if (scrollTop + clientHeight >= scrollHeight)
+						canScrollDownButtonEl.classList.add("unactive");
+					else canScrollDownButtonEl.classList.remove("unactive");
 				};
 			}
 		} catch (error) {
-			console.log("handleScrollHeader: ", error)
+			console.log("handleScrollHeader: ", error);
 		}
-	}
-}
+	},
+};
 
 document.addEventListener("DOMContentLoaded", () => Modules.onInt());
