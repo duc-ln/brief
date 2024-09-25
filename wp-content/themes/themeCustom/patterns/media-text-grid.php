45cac17c74ace64e31f1d1a880193ecdaeedd560
mume_ducln <ducln@mumesoft.com>
2024-09-25 22:57:54 +0700

    feat: create information and media text grid product partterns

---
+++ wp-content/themes/themeCustom/patterns/media-text-grid.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Title: Media Text Grid
+ * Slug: themeCustom/media-text-grid
+ * Categories: text
+ * Keywords: Media Text Grid
+ * Description: Media Text Grid
+ */
+?>
+
+<!-- wp:group {"className":"media-text-grid","layout":{"type":"grid","columnCount":2,"minimumColumnWidth":null}} -->
+<div class="wp-block-group media-text-grid"><!-- wp:group {"className":"media-text-grid__item","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group media-text-grid__item"><!-- wp:group {"className":"media-text-grid__image","layout":{"type":"default"}} -->
+        <div class="wp-block-group media-text-grid__image"><!-- wp:group {"className":"media-text-grid__image-emty","style":{"layout":{"columnSpan":3}},"backgroundColor":"gray-500","layout":{"type":"default"}} -->
+            <div class="wp-block-group media-text-grid__image-emty has-gray-500-background-color has-background"></div>
+            <!-- /wp:group -->
+
+            <!-- wp:image {"id":362,"sizeSlug":"full","linkDestination":"none","style":{"layout":{"columnSpan":6}}} -->
+            <figure class="wp-block-image size-full"><img src="http://interior.test:3000/wp-content/uploads/2024/09/image-12.png" alt="" class="wp-image-362" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:paragraph -->
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac augue at erat hendrerit dictum. Praesent porta, purus eget sagittis imperdiet, nulla mi ullamcorper metus, id hendrerit metus diam vitae est. Morbi suscipit interdum molestie. Aenean fringilla dui magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph -->
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac augue at erat hendrerit dictum. Praesent porta, purus eget sagittis imperdiet, nulla mi ullamcorper metus, id hendrerit metus diam vitae est. Morbi suscipit interdum molestie. Aenean fringilla dui magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"media-text-grid__item","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group media-text-grid__item"><!-- wp:paragraph -->
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac augue at erat hendrerit dictum. Praesent porta, purus eget sagittis imperdiet, nulla mi ullamcorper metus, id hendrerit metus diam vitae est. Morbi suscipit interdum molestie. Aenean fringilla dui magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph -->
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac augue at erat hendrerit dictum. Praesent porta, purus eget sagittis imperdiet, nulla mi ullamcorper metus, id hendrerit metus diam vitae est. Morbi suscipit interdum molestie. Aenean fringilla dui magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:group {"className":"media-text-grid__image","layout":{"type":"constrained"}} -->
+        <div class="wp-block-group media-text-grid__image"><!-- wp:group {"className":"media-text-grid__image-emty","style":{"layout":{"columnSpan":3}},"backgroundColor":"gray-500","layout":{"type":"default"}} -->
+            <div class="wp-block-group media-text-grid__image-emty has-gray-500-background-color has-background"></div>
+            <!-- /wp:group -->
+
+            <!-- wp:image {"id":362,"sizeSlug":"full","linkDestination":"none","style":{"layout":{"columnSpan":6}}} -->
+            <figure class="wp-block-image size-full"><img src="http://interior.test:3000/wp-content/uploads/2024/09/image-12.png" alt="" class="wp-image-362" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
---
+++ wp-content/themes/themeCustom/patterns/product-information.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Title: Product information
+ * Slug: themeCustom/product-information
+ * Categories: featured
+ * Keywords: Product information
+ * Description: Product information
+ */
+?>
+
+<!-- wp:group {"className":"product-information","layout":{"type":"default"}} -->
+<div class="wp-block-group product-information"><!-- wp:group {"className":"product-information__main","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group product-information__main"><!-- wp:heading {"textAlign":"center","level":1,"className":"product-information__title","style":{"elements":{"link":{"color":{"text":"var:preset|color|black-800"}}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"black-800","fontSize":"max"} -->
+        <h1 class="wp-block-heading has-text-align-center product-information__title has-black-800-color has-text-color has-link-color has-max-font-size" style="font-style:normal;font-weight:700">Lorem ipsum dolor</h1>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"align":"center","className":"product-information__description","style":{"elements":{"link":{"color":{"text":"var:preset|color|gray-800"}}}},"textColor":"gray-800","fontSize":"s"} -->
+        <p class="has-text-align-center product-information__description has-gray-800-color has-text-color has-link-color has-s-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac augue at erat hendrerit dictum. Praesent porta, purus eget sagittis imperdiet, nulla mi ullamcorper metus, id hendrerit metus diam vitae est. Morbi suscipit interdum molestie. Aenean fringilla dui magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:group {"className":"product-information__content","layout":{"type":"grid","columnCount":4,"minimumColumnWidth":null}} -->
+        <div class="wp-block-group product-information__content"><!-- wp:group {"className":"product-information__item","style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group product-information__item"><!-- wp:paragraph {"className":"product-information__item-label","style":{"elements":{"link":{"color":{"text":"var:preset|color|black-800"}}}},"textColor":"black-800","fontSize":"s"} -->
+                <p class="product-information__item-label has-black-800-color has-text-color has-link-color has-s-font-size">Year</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:separator {"backgroundColor":"gray-800"} -->
+                <hr class="wp-block-separator has-text-color has-gray-800-color has-alpha-channel-opacity has-gray-800-background-color has-background" />
+                <!-- /wp:separator -->
+
+                <!-- wp:paragraph {"className":"product-information__item-content","style":{"elements":{"link":{"color":{"text":"var:preset|color|black-800"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"black-800","fontSize":"xl"} -->
+                <p class="product-information__item-content has-black-800-color has-text-color has-link-color has-xl-font-size" style="font-style:normal;font-weight:600">2024</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group {"className":"product-information__item","style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group product-information__item"><!-- wp:paragraph {"className":"product-information__item-label","style":{"elements":{"link":{"color":{"text":"var:preset|color|black-800"}}}},"textColor":"black-800","fontSize":"s"} -->
+                <p class="product-information__item-label has-black-800-color has-text-color has-link-color has-s-font-size">Location</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:separator {"backgroundColor":"gray-800"} -->
+                <hr class="wp-block-separator has-text-color has-gray-800-color has-alpha-channel-opacity has-gray-800-background-color has-background" />
+                <!-- /wp:separator -->
+
+                <!-- wp:paragraph {"className":"product-information__item-content","style":{"elements":{"link":{"color":{"text":"var:preset|color|black-800"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"black-800","fontSize":"xl"} -->
+                <p class="product-information__item-content has-black-800-color has-text-color has-link-color has-xl-font-size" style="font-style:normal;font-weight:600">Location</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group {"className":"product-information__item","style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group product-information__item"><!-- wp:paragraph {"className":"product-information__item-label","style":{"elements":{"link":{"color":{"text":"var:preset|color|black-800"}}}},"textColor":"black-800","fontSize":"s"} -->
+                <p class="product-information__item-label has-black-800-color has-text-color has-link-color has-s-font-size">Creative Director</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:separator {"backgroundColor":"gray-800"} -->
+                <hr class="wp-block-separator has-text-color has-gray-800-color has-alpha-channel-opacity has-gray-800-background-color has-background" />
+                <!-- /wp:separator -->
+
+                <!-- wp:paragraph {"className":"product-information__item-content","style":{"elements":{"link":{"color":{"text":"var:preset|color|black-800"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"black-800","fontSize":"xl"} -->
+                <p class="product-information__item-content has-black-800-color has-text-color has-link-color has-xl-font-size" style="font-style:normal;font-weight:600">Andrus' Bezden</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group {"className":"product-information__item","style":{"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group product-information__item"><!-- wp:paragraph {"className":"product-information__item-label","style":{"elements":{"link":{"color":{"text":"var:preset|color|black-800"}}}},"textColor":"black-800","fontSize":"s"} -->
+                <p class="product-information__item-label has-black-800-color has-text-color has-link-color has-s-font-size">Vizualization</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:separator {"backgroundColor":"gray-800"} -->
+                <hr class="wp-block-separator has-text-color has-gray-800-color has-alpha-channel-opacity has-gray-800-background-color has-background" />
+                <!-- /wp:separator -->
+
+                <!-- wp:paragraph {"className":"product-information__item-content","style":{"elements":{"link":{"color":{"text":"var:preset|color|black-800"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"black-800","fontSize":"xl"} -->
+                <p class="product-information__item-content has-black-800-color has-text-color has-link-color has-xl-font-size" style="font-style:normal;font-weight:600">Sasha Hamolin</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
